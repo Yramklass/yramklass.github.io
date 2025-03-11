a1f970f6bfab4da7ef6adf7521416b3e4dfddcec
Yramklass <yramklass@gmail.com>
2025-03-11 18:31:53 +0200

    footer opens CV in new tab

--- webcv/src/components/FooterComponent.jsx
+++ webcv/src/components/FooterComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FooterComponent = () => {
   return (
-    <div style={{textAlign:'center'}} className='footer'>| 2025 - Yash Ramklass - All rights reserved. | <a href='#'>Download CV</a></div>
+    <div style={{textAlign:'center'}} className='footer'>| 2025 - Yash Ramklass - All rights reserved. | <a href='Yash_CV_2025.pdf' target='_blank'>Download CV</a></div>
   )
 }
 
