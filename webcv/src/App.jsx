b2aaf2d8e1516efb4deee87d1edcd6a0b345fd29
Yramklass <yramklass@gmail.com>
2025-04-13 15:23:47 +0200

    Added mobile responsiveness for projects showcase, header

--- webcv/src/App.jsx
+++ webcv/src/App.jsx
@@ -1,14 +1,12 @@
 
 import './App.css'
-import AboutComponent from './components/AboutComponent'
-import BioComponent from './components/BioComponent'
-import ContactComponent from './components/ContactComponent'
+
 import ContactSection from './components/ContactSection'
 import DetailsSection from './components/DetailsSection'
-import DigitalNoticeBoard from './components/DigitalNoticeBoard'
+
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
-import ProjectsComponent from './components/ProjectsComponent'
+
 import ProjectShowcase from './components/ProjectShowcase'
 
 
@@ -18,13 +16,11 @@ function App() {
     <>
       <div>
         <HeaderComponent/>
-        {/* <BioComponent/> */}
-        {/* <AboutComponent/> */}
+
         <DetailsSection/>
         <ProjectShowcase/>
         <ContactSection/>
-        {/* <ContactComponent/> */}
-        {/* <DigitalNoticeBoard/> */}
+
         <FooterComponent/>
 
       </div>
--- webcv/src/components/HeaderComponent.jsx
+++ webcv/src/components/HeaderComponent.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './Header.css';
 import { Link as ScrollLink } from 'react-scroll';
 
-
 const HeaderComponent = () => {
   const [scrollY, setScrollY] = useState(0);
   const [scrollDir, setScrollDir] = useState("up");
@@ -27,16 +26,13 @@ const HeaderComponent = () => {
           <img src="signature_white.png" alt="Yash Signature" className="signature-logo" />
         </div>
         <div className="nav-right">
-        <ScrollLink to="about" spy={true} smooth={true} duration={500} offset={-80} activeClass="active">About</ScrollLink>
-<ScrollLink to="education" spy={true} smooth={true} duration={500} offset={-80} activeClass="active">Education</ScrollLink>
-<ScrollLink to="experience" spy={true} smooth={true} duration={500} offset={-80} activeClass="active">Experience</ScrollLink>
-<ScrollLink to="projects" spy={true} smooth={true} duration={500} offset={-80} activeClass="active">Projects</ScrollLink>
-<ScrollLink to="contact" spy={true} smooth={true} duration={500} offset={-80} activeClass="active">Contact</ScrollLink>
-
-          
+          <ScrollLink to="about" spy smooth duration={500} offset={-80} activeClass="active">About</ScrollLink>
+          <ScrollLink to="education" spy smooth duration={500} offset={-80} activeClass="active">Education</ScrollLink>
+          <ScrollLink to="experience" spy smooth duration={500} offset={-80} activeClass="active">Experience</ScrollLink>
+          <ScrollLink to="projects" spy smooth duration={500} offset={-80} activeClass="active">Projects</ScrollLink>
+          <ScrollLink to="contact" spy smooth duration={500} offset={-80} activeClass="active">Contact</ScrollLink>
         </div>
       </nav>
-
       <header className="headerBox">
         <div className="header-center">
           <h1>Yash Ramklass</h1>
